67bc068d2be5bc66b047ece9882a00143a899939
Benson Yan <bensonyan778@hotmail.com>
2024-07-12 14:22:36 -0400

    updated

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,29 @@
 import express from 'express';
-
+import { query, body, validationResult, matchedData, checkSchema } from 'express-validator' // for validating query parameters
+import { createUserValidationSchema } from './utils/validationSchemas.mjs';
 const app = express();
 
 app.use(express.json()) // register middleware
 
+const loggingMiddleware = (request, response, next) => { // next is what to do after middleware
+    console.log(`${request.method} - ${request.url}`); // ie: GET - /
+    next();
+};
+
 const PORT = process.env.PORT || 3000;
+
+const resolveIndexByUserId = (request, response, next) => {
+    const { params: { id }, } = request; // deconstruct body and params's id
+
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) return response.sendStatus(400);
+
+    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId) // we converted it to numeric instead of a number string
+
+    if (findUserIndex === -1) return response.sendStatus(404) // returns -1 if doesn't exist
+    request.findUserIndex = findUserIndex // future middlewares can use findUserIndex property
+    next();
+}
 
 const mockUsers = [
     { id: 1, username: "anson", displayName: "Anson" },
@@ -17,49 +36,72 @@ const mockUsers = [
 ];
 
 // second argument is a callback function with a request and response object
-app.get('/', (request, response) => {
+app.get('/', 
+    (request, response, next) => {
+        console.log("base url 1");
+        next()
+    },
+    (request, response, next) => {
+        console.log("base url 2");
+        next()
+    }, (request, response, next) => {
+        console.log("base url 3");
+        next()
+    }, (request, response) => {
     response.status(201).send({ msg: "hello world" });
 });
 
+
 // use /api/... 
 // localhost:3000/api/users?filter=username&value=an
-app.get('/api/users', (request, response) => {
-    console.log(request.query);
-    const { query: { filter, value } } = request;
+app.get('/api/users', query('filter')
+    .isString()
+    .notEmpty()
+    .withMessage('must not be empty')
+    .isLength({ min: 3, max: 10 })
+    .withMessage('must be 3-10 characters'),
 
+    (request, response) => {
+        const result = validationResult(request);
+        console.log(result)
+        const { query: { filter, value } } = request;
 
-    if (filter && value) return response.send(
-        mockUsers.filter((user) => user[filter].includes(value))
-    );
+        if (filter && value) return response.send(
+            mockUsers.filter((user) => user[filter].includes(value))
+        );
 
     // when the filter is undefined
     return response.send(mockUsers);
 });
 
+app.use(loggingMiddleware, (request, response, next) => { // calling 2 middlewares
+    console.log("Finished logging...");
+    next()
+});
+
 
 // post requests have data in its 'request body' or 'payload'
-app.post('/api/users', (request, response) => {
-    const { body } = request; // takes only the body from request
-    console.log(request.body); // now prints only the body
-    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body};
-    mockUsers.push(newUser)
+app.post('/api/users', checkSchema(createUserValidationSchema),
+    (request, response) => {
+        const result = validationResult(request);
+        console.log(result)
+        
+        if(!result.isEmpty()) return response.status(400).send({ errors: result.array() });
 
-    return response.status(201).send(newUser)
-})
+        const data = matchedData(request); // use this matchedData instead of request.body
+        console.log(data);
+
+        const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...data}; // returns rest of user like whatever u sent
+        mockUsers.push(newUser); // push onto the database
+
+        return response.status(201).send(newUser);
+    }
+);
 
 
-app.post
-
-app.get('/api/users/:id', (request, response) => {
-    console.log(request.params); // tells us the parameter id
-    const parsedId = parseInt(request.params.id); // is it valid?
-
-    // if invalid, return 400
-    if (isNaN(parsedId)) return response.status(400).send({ msg: "bad request. invalid id"}); 
-    
-    // find the user
-    const findUser = mockUsers.find((user) => user.id === parsedId);
-
+app.get('/api/users/:id', resolveIndexByUserId, (request, response) => {
+    const { findUserIndex } = request;
+    const findUser = mockUsers[findUserIndex]
     if (!findUser) return response.sendStatus(404);
     return response.send(findUser)
 })
@@ -68,9 +110,27 @@ app.get('/api/products', (request, response) => {
     response.send([{ id: 123, name: "chicken breast", price: 12.99}])
 });
 
+app.put('/api/users/:id', resolveIndexByUserId, (request, response) => {
+    const { body, findUserIndex } = request; // keep request body
 
+    mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body } // updates user with whatever you passed in
+    return response.sendStatus(200);
+});
 
+app.patch('/api/users/:id', resolveIndexByUserId, (request, response) => {
+    const { body, findUserIndex } = request;
 
+    mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body } // keep the rest except whatever you send
+    return response.sendStatus(200);
+});
+
+app.delete('/api/users/:id', resolveIndexByUserId, (request, response) => { // usually dont need a body
+    const {  findUserIndex, } = request;
+
+    mockUsers.splice(findUserIndex, 1); // deletes only 1
+
+    return response.sendStatus(200);
+})
 
 
 
